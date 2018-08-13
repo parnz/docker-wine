8ec47310c3b5fff4c5edd603c61f2eee9b77bab8
Wisitchai Supakad <1014336+parnz@users.noreply.github.com>
2018-08-13 20:44:19 +0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
-openbox-session&
+openbox-session &
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-cd /home/wine/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
+cd /home/wine/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900 &
